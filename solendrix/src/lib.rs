78580e2726e817fe9aa300511b42b8c918375b37
Darren <darren26feb@gmail.com>
2025-08-29 23:23:02 +0530

    Initial Structure

---
+++ solendrix/src/lib.rs
@@ -0,0 +1,16 @@
+use pinocchio::{entrypoint, instruction, pubkey::Pubkey, program_error::ProgramError, AccountInfo, ProgramResult};
+entrypoint!(process_instruction);
+
+pinocchio_pubkey::declare_id!("Dr2Y39b8JDWbmvug8UPwQvorfsEkyog9r4AR3ZcK5cJU");
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    assert_eq!(program_id, &ID);
+    match instruction_data.split_first() {
+            Some(Deposit) => todo!(),
+            _ => Err(ProgramError::InvalidInstructionData),
+    }
+}
---
+++ solendrix/target/debug/deps/sha2_const_stable-b9ab2774144a6615.d
@@ -0,0 +1,8 @@
+/home/darren/Desktop/solana-proj/solendrix/target/debug/deps/sha2_const_stable-b9ab2774144a6615.d: /home/darren/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha2-const-stable-0.1.0/src/lib.rs /home/darren/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha2-const-stable-0.1.0/src/constants.rs /home/darren/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha2-const-stable-0.1.0/src/sha.rs /home/darren/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha2-const-stable-0.1.0/src/util.rs
+
+/home/darren/Desktop/solana-proj/solendrix/target/debug/deps/libsha2_const_stable-b9ab2774144a6615.rmeta: /home/darren/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha2-const-stable-0.1.0/src/lib.rs /home/darren/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha2-const-stable-0.1.0/src/constants.rs /home/darren/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha2-const-stable-0.1.0/src/sha.rs /home/darren/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha2-const-stable-0.1.0/src/util.rs
+
+/home/darren/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha2-const-stable-0.1.0/src/lib.rs:
+/home/darren/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha2-const-stable-0.1.0/src/constants.rs:
+/home/darren/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha2-const-stable-0.1.0/src/sha.rs:
+/home/darren/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha2-const-stable-0.1.0/src/util.rs:
