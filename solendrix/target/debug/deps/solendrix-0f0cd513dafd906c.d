5dd582ca6ae3bfd1b20f9e8131d5b02217cf9cc5
Darren <darren26feb@gmail.com>
2025-08-29 23:40:34 +0530

    fix

--- solendrix/target/debug/deps/solendrix-0f0cd513dafd906c.d
+++ solendrix/target/debug/deps/solendrix-0f0cd513dafd906c.d
@@ -1,5 +1,5 @@
-/home/darren/Desktop/solana-proj/solendrix/target/debug/deps/solendrix-0f0cd513dafd906c.d: src/lib.rs
+/home/darren/Desktop/solana-proj/solendrix/solendrix/target/debug/deps/solendrix-0f0cd513dafd906c.d: src/lib.rs
 
-/home/darren/Desktop/solana-proj/solendrix/target/debug/deps/libsolendrix-0f0cd513dafd906c.rmeta: src/lib.rs
+/home/darren/Desktop/solana-proj/solendrix/solendrix/target/debug/deps/libsolendrix-0f0cd513dafd906c.rmeta: src/lib.rs
 
 src/lib.rs:
--- solendrix/target/debug/deps/solendrix-6672cfcf10efc3db.d
+++ solendrix/target/debug/deps/solendrix-6672cfcf10efc3db.d
@@ -1,5 +1,5 @@
-/home/darren/Desktop/solana-proj/solendrix/target/debug/deps/solendrix-6672cfcf10efc3db.d: src/lib.rs
+/home/darren/Desktop/solana-proj/solendrix/solendrix/target/debug/deps/solendrix-6672cfcf10efc3db.d: src/lib.rs
 
-/home/darren/Desktop/solana-proj/solendrix/target/debug/deps/libsolendrix-6672cfcf10efc3db.rmeta: src/lib.rs
+/home/darren/Desktop/solana-proj/solendrix/solendrix/target/debug/deps/libsolendrix-6672cfcf10efc3db.rmeta: src/lib.rs
 
 src/lib.rs:
